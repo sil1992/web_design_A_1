70c01ac4de19f10a39c40035d594235d8cbe674e
sil1992 <ehddhksdlsms@gmail.com>
2022-10-28 01:14:20 +0900

    221028-1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-// Menu
+//Menu
 $('#header .gnb ul li ul').hide();
+//서브메뉴 숨기기
 $('#header .gnb>ul>li>a').mouseenter(function() {
-    $(this).addClass('on');  
-    $('#header .gnb ul li ul').stop().slideDown(500);    
-});
+    $(this).addClass('on');
+    //메인메뉴 리스트에 마우스를 올리면 미리 설정해둔 on클래스가 적용
+    $('#header .gnb ul li ul').stop().slideDown(500);
+}); //메인메뉴 리스트에 마우스를 올리면 서브메뉴가 등장(5초)
 $('#header .gnb>ul>li').mouseleave(function() {
     $('#header .gnb>ul>li>a').removeClass('on');
+    //메인메뉴 리스트에서 마우스를 떼면 on클래스 해지
     $('#header .gnb ul li ul').stop().slideUp(300);
-}); 
+    //메인메뉴 리스트에서 마우스를 떼면 서브메뉴가 사라짐(3초)
+});
 
-// Slider
+//Slider
 var imgs = $('#slider > ul > li').length -1;
 var h = $('#slider > ul > li').height();
 var now = 0;
@@ -20,32 +24,23 @@ setInterval(function() {
     });
 }, 2500);
 
-// 가로 슬라이드
-// var imgs = $('#slider > ul > li').length -1;
-// var w = $('#slider > ul > li').width();
-// var now = 0;
-// setInterval(function() {
-//     now = (now == imgs) ? 0 : now + 1;
-//     $('#slider > ul').animate({
-//         left : -(w * now)
-//     });
-// }, 2500);
-
-// Tab Menu
+//Tab Menu
 $('.news .gallery').hide();
-$('.news .title').click(function() {
+$('.news .title').click(function(){
     $(this).addClass('on')
         .siblings('.title').removeClass('on');
     $(this).next('ul').show()
         .siblings('ul').hide();
 });
 
-// Popup
+//Popup
 $('.bg, .popup').hide();
-$('.notice .active').click(function(e) {
+$('.notice .active').click(function(e){
     e.preventDefault();
     $('.bg, .popup').fadeIn(500);
+    //공지사항에 설정했던 .active를 클릭하면 배경과 팝업이 0.5초 속도로 보여짐
 });
-$('.close').click(function() {
+$('.close').click(function(){
     $('.bg, .popup').fadeOut(300);
+    //팝업에 닫기버튼을 누르면 배경과 팝업이 0.3초 속도로 사라짐
 });
